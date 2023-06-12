374474b06f267d64ee37c21ddebbabde36d84633
Mark James <mail@mark.james.name>
2023-06-12 12:50:06 +0100

    Change game state menu to use PrimeVue breadcrumbs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+import Breadcrumb from "primevue/Breadcrumb"
+import { ref } from 'vue';
+
+const router = useRouter();
+
+const gameProgressQuicklinks = ref([
+  { label: 'Home', command: () => { router.push({ name: 'home' }) } },
+  { label: 'Lobby', command: () => { router.push({ name: 'lobby' }) } },
+  { label: 'Game', command: () => { router.push({ name: 'game' }) } }
+]);
+console.log(router);
+
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/lobby">Lobby</RouterLink>
-        <RouterLink to="/game">Game</RouterLink>
-      </nav>
+      <Breadcrumb :model="gameProgressQuicklinks" />
     </div>
   </header>
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
