37002426025d63cd3ef948dcd172ea1adb12fde7
Mark James <mail@mark.james.name>
2023-06-12 15:09:34 +0100

    Add firebase database setup

---
+++ src/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { Database, get as get, getDatabase, ref } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAN2LMaWnYgr_vLO1-_3gKhKSCGjaiiUbo",
+  authDomain: "spectrumgame-c8131.firebaseapp.com",
+  projectId: "spectrumgame-c8131",
+  storageBucket: "spectrumgame-c8131.appspot.com",
+  messagingSenderId: "669865437548",
+  appId: "1:669865437548:web:70074627afee9c66b0eb49",
+  databaseURL: "https://spectrumgame-c8131-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// used for the firestore refs
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
+const db:Database = getDatabase(firebaseApp);
+export const firebaseRef = ref;
+export const firebaseGet = get;
+export const questionsRef = firebaseRef(db, 'questions');
+export const gamesRef = firebaseRef(db, 'games');
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueFire } from 'vuefire'
 import PrimeVue from 'primevue/config';
 import "primevue/resources/primevue.min.css";
 import "primevue/resources/themes/lara-light-indigo/theme.css";  ; 
 
 import App from './App.vue'
 import router from './router'
+import { firebaseApp } from './firebase'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(VueFire, {firebaseApp});
 app.use(PrimeVue)
 app.use(router)
 
